998bc3bfa1afb4396b12a5aed9052b335e4490f3
James Ravenscroft <james@jamesravey.me>
2024-11-13 21:21:37 +0000

    implementing reg validation

--- src/annomemo/webapp/views/auth.py
+++ src/annomemo/webapp/views/auth.py
@@ -1,7 +1,7 @@
 from ..app import app, templates
 from ..db import UserInDB
 
-from pydantic import BaseModel
+from pydantic import BaseModel, EmailStr, field_validator
 from typing import Annotated
 from fastapi import APIRouter, Request, Form
 
@@ -9,15 +9,23 @@ authapi = APIRouter()
 
 
 class RegistrationForm(BaseModel):
-    email: str
+    fullName: str
+    email: EmailStr
     password: str
+    confirmPassword: str
+
+    @field_validator('confirmPassword')
+    def passwords_match(cls, v, info):
+        if v != info.data.get('password'):
+            raise ValueError('Passwords do not match')
+        return v
 
 
 @authapi.get("/register")
 async def register_form(req: Request):
     """Display registration form"""
 
-    return templates.TemplateResponse(req, "register.html", {})
+    return templates.TemplateResponse(req, "register.html", {"errors": {}})
 
 
 @authapi.post("/register", )
